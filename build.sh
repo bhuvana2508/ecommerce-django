87046645e9452ff352d0dd2caa93044f9012d35c
bhuvana2508 <bhuvaneshwaribalu1996@gmail.com>
2026-06-17 14:25:25 +0530

    fix: recreate superuser in build

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-mkdir -p staticfiles
 python manage.py collectstatic --no-input
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell
